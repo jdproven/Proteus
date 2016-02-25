bf558acdfdc0f126888c40d5f047686223e2c2b0
jdproven <jdproven@users.noreply.github.com>
2016-02-25 01:40:51 -0600

    Delete build.bash

--- SnappyBird/build.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-javac Game.java Model.java View.java Controller.java
-echo "Build complete. To run, do:"
-echo "    java Game"
-echo "and start tapping keys or your mouse button."
